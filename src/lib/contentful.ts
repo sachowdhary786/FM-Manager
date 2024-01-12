8cc16a171667a309a808077f467c5931a0a5441a
Saad Chowdhary <sachowdhary786@gmail.com>
2024-01-12 23:54:19 +0000

    Base Card Playground in place
    Card Playground is in place
    Data and functions work on the server side
    Data isn't pulling through to the data side

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,4 +1,4 @@
-import contenful, {type EntryFieldTypes} from "contentful";
+import contentful, {type EntryFieldTypes} from "contentful";
 
 export interface PlayerType {
     contentTypeId: "playerType",
@@ -29,7 +29,9 @@ export interface SquadType {
     }
 }
 
-export const contentfulClient = contenful.createClient({
+console.log(import.meta.env)
+
+export const contentfulClient = contentful.createClient({
     space: import.meta.env.CONTENTFUL_SPACE_ID,
     accessToken: import.meta.env.DEV ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
     host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com"
