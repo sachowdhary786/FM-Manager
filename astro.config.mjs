39c135a6d6e411f3737f2963ce901916a266aeba
Saad Chowdhary <sachowdhary786@gmail.com>
2024-01-18 14:43:08 +0000

    data dump
    Shits broken. Fix it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+import solidJs from "@astrojs/solid-js";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  define: {'import.meta.env': import.meta.env,},
-  integrations: [tailwind(), vue()]
+  define: {
+    'import.meta.env': import.meta.env
+  },
+  integrations: [tailwind(), vue(), solidJs(), svelte()]
 });
